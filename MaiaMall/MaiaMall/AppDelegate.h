9f6f45021b2d4fc94ddab5520ec2d99313e38eaa
Andreea-Ingrid Funie <andreeaingridf@gmail.com>
2015-03-11 19:50:02 +0000

    Initial commit

---
+++ MaiaMall/MaiaMall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MaiaMall
+//
+//  Created by James Cross on 11/03/2015.
+//  Copyright (c) 2015 James Cross. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MaiaMall/MaiaMall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MaiaMall
+//
+//  Created by James Cross on 11/03/2015.
+//  Copyright (c) 2015 James Cross. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
