796e6c789aaef115653185be29261d15e5ca3119
Andreea-Ingrid Funie <andreeaingridf@gmail.com>
2015-04-15 10:51:23 +0100

    added appointment

--- SidebarDemo/RetailDetailViewController.h
+++ SidebarDemo/RetailDetailViewController.h
@@ -9,14 +9,16 @@
 #import <UIKit/UIKit.h>
 #import "MMDStore.h"
 #import "ALAlertBanner.h"
+#import "THDatePickerViewController.h"
 
-@interface RetailDetailViewController : UIViewController<UIGestureRecognizerDelegate, UITabBarControllerDelegate, UITabBarDelegate>
+@interface RetailDetailViewController : UIViewController<UIGestureRecognizerDelegate, UITabBarControllerDelegate, UITabBarDelegate, THDatePickerDelegate>
 @property (weak, nonatomic) IBOutlet UITabBar *tabBarController;
 @property (strong, nonatomic) IBOutlet UILabel *brandTitle;
 @property (strong, nonatomic) IBOutlet UITextView *brandDescription;
 @property (strong, nonatomic) IBOutlet UIImageView *brandImage;
 
 - (void)initWithStore:(MMDStore*)store;
+@property (nonatomic, strong) THDatePickerViewController * datePicker;
 
 - (IBAction)bookAnApptClicked:(UIButton *)sender;
 - (IBAction)offersClicked:(UIButton *)sender;
--- SidebarDemo/RetailDetailViewController.m
+++ SidebarDemo/RetailDetailViewController.m
@@ -11,6 +11,8 @@
 @interface RetailDetailViewController ()
 
 @property (strong, nonatomic) MMDStore * currentStore;
+@property (nonatomic, retain) NSDate * curDate;
+@property (nonatomic, retain) NSDateFormatter * formatter;
 
 @end
 
@@ -57,6 +59,10 @@
     self.brandTitle.text = self.currentStore.storeTitle;
     self.brandDescription.text = self.currentStore.storeDescription;
     self.brandImage.image = self.currentStore.storeLogo;
+    
+    self.curDate = [NSDate date];
+    self.formatter = [[NSDateFormatter alloc] init];
+    [_formatter setDateFormat:@"dd/MM/yyyy --- HH:mm"];
 
     
 }
@@ -123,6 +129,59 @@
 
 - (IBAction)bookAnApptClicked:(UIButton *)sender {
     
+    
+    if(!self.datePicker)
+        self.datePicker = [THDatePickerViewController datePicker];
+    self.datePicker.date = self.curDate;
+    self.datePicker.delegate = self;
+    [self.datePicker setAllowClearDate:NO];
+    [self.datePicker setClearAsToday:YES];
+    [self.datePicker setAutoCloseOnSelectDate:NO];
+    [self.datePicker setAllowSelectionOfSelectedDate:YES];
+    [self.datePicker setDisableHistorySelection:YES];
+    [self.datePicker setDisableFutureSelection:NO];
+    //[self.datePicker setAutoCloseCancelDelay:5.0];
+    [self.datePicker setSelectedBackgroundColor:[UIColor colorWithRed:125/255.0 green:208/255.0 blue:0/255.0 alpha:1.0]];
+    [self.datePicker setCurrentDateColor:[UIColor colorWithRed:242/255.0 green:121/255.0 blue:53/255.0 alpha:1.0]];
+    [self.datePicker setCurrentDateColorSelected:[UIColor yellowColor]];
+    
+    [self.datePicker setDateHasItemsCallback:^BOOL(NSDate *date) {
+        int tmp = (arc4random() % 30)+1;
+        return (tmp % 5 == 0);
+    }];
+    
+   // [self.datePicker slideUpInView:self.view withModalColor:[UIColor lightGrayColor]];
+    
+  //  [self presentViewController:self.datePicker animated:YES completion:nil];
+    
+    [self presentSemiViewController:self.datePicker withOptions:@{
+                                                                  KNSemiModalOptionKeys.pushParentBack    : @(NO),
+                                                                  KNSemiModalOptionKeys.animationDuration : @(1.0),
+                                                                  KNSemiModalOptionKeys.shadowOpacity     : @(0.3),
+                                                                  }];
+
+    
+    
+   /* ALAlertBanner *banner = [ALAlertBanner alertBannerForView:self.view
+                                                        style:ALAlertBannerStyleNotify
+                                                     position:ALAlertBannerPositionTop
+                                                        title:@"Welcome"
+                                                     subtitle:@"You appointment have been succesfully booked. We welcome you with joy!"
+                                                        image:nil
+                                                  tappedBlock:nil];
+    [banner setSecondsToShow:0];
+    [banner show];*/
+    
+    //    UIAlertView * alertView = [[UIAlertView alloc] initWithTitle:@"Welcome" message:@"You appointment have been succesfully booked. We welcome you with joy!" delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
+    //    [alertView show];
+}
+
+- (void)datePickerDonePressed:(THDatePickerViewController *)datePicker {
+    self.curDate = datePicker.date;
+  //  [self refreshTitle];
+    //[self.datePicker slideDownAndOut];
+    [self dismissSemiModalView];
+    
     ALAlertBanner *banner = [ALAlertBanner alertBannerForView:self.view
                                                         style:ALAlertBannerStyleNotify
                                                      position:ALAlertBannerPositionTop
@@ -132,12 +191,16 @@
                                                   tappedBlock:nil];
     [banner setSecondsToShow:0];
     [banner show];
-    
-    //    UIAlertView * alertView = [[UIAlertView alloc] initWithTitle:@"Welcome" message:@"You appointment have been succesfully booked. We welcome you with joy!" delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
-    //    [alertView show];
 }
 
+- (void)datePickerCancelPressed:(THDatePickerViewController *)datePicker {
+    //[self.datePicker slideDownAndOut];
+    [self dismissSemiModalView];
+}
 
+- (void)datePicker:(THDatePickerViewController *)datePicker selectedDate:(NSDate *)selectedDate {
+    NSLog(@"Date selected: %@",[_formatter stringFromDate:selectedDate]);
+}
 
 /*
 #pragma mark - Navigation
